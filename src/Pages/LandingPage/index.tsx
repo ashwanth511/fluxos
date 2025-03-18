0977d93fb6b42b80461ceb7408a684504bed4517
ashwanth511 <ashwanthstark1963@gmail.com>
2025-03-18 09:09:54 +0530

    all features

--- src/Pages/LandingPage/index.tsx
+++ src/Pages/LandingPage/index.tsx
@@ -84,19 +84,19 @@ export default function LandingPage() {
         <div className="container mx-auto max-w-6xl relative">
           <div className="text-center mb-16">
             <h1 className="font-space-grotesk text-6xl md:text-8xl font-bold mb-6 relative">
-              Manage your wall<span className="text-red-500">*</span>t easily!
+              Unleash The Power of AI<span className="text-red-500">*</span>
               <span className="text-blue-500">{"{ INJective }"}</span>
               <div className="absolute -right-8 top-0 w-20 h-20 bg-blue-500/10 rounded-full blur-2xl"></div>
               <div className="absolute -left-8 bottom-0 w-20 h-20 bg-purple-500/10 rounded-full blur-2xl"></div>
             </h1>
             <p className="text-gray-500 text-lg mb-8 font-space-grotesk">
-             One Single OS for all yur trading needs From DeFi to Automations,Build For <span className="text-violet-800">{"{Injective}"}</span> 
+             One Single OS for all your needs From DeFi Sawning Agents, Automations and More ....,Build For <span className="text-violet-800">{"{Injective}"}</span> 
             </p>
             <div className="flex items-center justify-center gap-4">
               <Link to="/dashboard"><Button size="lg" className="rounded-md px-8 bg-black text-white hover:bg-blue-700 font-space-grotesk">
                 Get Started 
               </Button> </Link>
-              <Link to="https://youtu.be/0vS2Gt2MnYk"><Button variant="ghost" size="lg" className="rounded-md gap-2 font-space-grotesk">
+              <Link to="https://youtube.com/"><Button variant="ghost" size="lg" className="rounded-md gap-2 font-space-grotesk">
                 <Play className="w-4 h-4" /> Watch Demo
               </Button></Link>
             </div>
